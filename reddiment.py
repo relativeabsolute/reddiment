37a6b37f12a1323f9b1596dbde0bce701e87364c
Johan Burke <jburke4126@gmail.com>
2019-08-10 17:45:18 -0400

    Added scatter plotting of comment measures.  Next step is to normalize measurements and build linear regression models.

--- reddiment.py
+++ reddiment.py
@@ -1,6 +1,8 @@
 import argparse
 import requests
 from textblob import TextBlob
+import numpy as np
+import matplotlib.pyplot as plt
 
 is_verbose = False
 
@@ -15,19 +17,24 @@ session = requests.Session()
 session.headers.update({'user-agent': 'linux:reddiment:v1.0 (by /u/relativeabsolute)'})
 
 
+# return a tuple containing (polarity, subjectivity, score)
 def analyze_comment(comment):
 	verbose_print("Comment author - {}".format(comment['data']['author']))
 	body = comment['data']['body']
+	score = comment['data']['score']
 	verbose_print("Comment body - {}".format(body))
-	verbose_print("Comment score - {}".format(comment['data']['score']))
+	verbose_print("Comment score - {}".format(score))
 	blob = TextBlob(body)
 	sentiment = blob.sentiment
 	verbose_print("Comment analysis: polarity = {} and subjectivity = {}".format(sentiment[0], sentiment[1]))
+	return (sentiment[0], sentiment[1], score)
 
 
+# return a list of tuples containing (polarity, subjectivity, score) for each comment on the post
 def analyze_post_comments(post_data):
 	verbose_print("Post title - {}".format(post_data['title']))
-	verbose_print("Number of comments - {}".format(post_data['num_comments']))
+	num_comments = post_data['num_comments']
+	verbose_print("Number of comments - {}".format(num_comments))
 	permalink = post_data["permalink"]
 	verbose_print("Permalink - {}".format(permalink))
 	url = "https://www.reddit.com{}.json".format(permalink)
@@ -35,13 +42,15 @@ def analyze_post_comments(post_data):
 	post_response = session.get(url)
 	post_response.raise_for_status()
 	response_json = post_response.json()
+	post_scores = []
 	for comment in response_json[1]['data']['children']:
 		if comment['kind'] == 't1':
-			analyze_comment(comment)
+			post_scores.append(analyze_comment(comment))
 		else:
 			# last item in a comments listing is a 'more' item
 			# TODO: figure out how to handle it
 			pass
+	return post_scores
 	
 
 if __name__ == "__main__":
@@ -61,5 +70,24 @@ if __name__ == "__main__":
 	response.raise_for_status()
 	response_json = response.json()
 	verbose_print("Number of posts = {}".format(response_json['data']['dist']))
+	all_scores = []
 	for post in response_json['data']['children']:
-		analyze_post_comments(post['data'])
+		 all_scores.extend(analyze_post_comments(post['data']))
+	as_np = np.array(all_scores)
+	plt.figure()
+	plt.scatter(as_np[:,0], as_np[:,1])
+	plt.xlabel('polarity')
+	plt.ylabel('subjectivity')
+	plt.savefig('polarityvssubjectivity.png')
+
+	plt.figure()
+	plt.scatter(as_np[:,0], as_np[:,2])
+	plt.xlabel('polarity')
+	plt.ylabel('score')
+	plt.savefig('polarityvsscore.png')
+
+	plt.figure()
+	plt.scatter(as_np[:,1], as_np[:,2])
+	plt.xlabel('subjectivity')
+	plt.ylabel('score')
+	plt.savefig('subjectivityvsscore.png')
